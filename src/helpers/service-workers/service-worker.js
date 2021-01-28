93a7add454e57e2e43e025aebe29c4f2a204ddeb
rgualberto <itzdrigz@gmail.com>
2021-01-28 18:19:09 -0500

    add additional cache settings

--- src/helpers/service-workers/service-worker.js
+++ src/helpers/service-workers/service-worker.js
@@ -1,4 +1,11 @@
 import { cleanupOutdatedCaches,  precacheAndRoute} from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
+import {CacheFirst} from 'workbox-strategies';
 
 precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
+
+registerRoute(
+  ({request}) => request.destination === 'assets/images',
+  new CacheFirst({cacheName: 'images'}),
+);
